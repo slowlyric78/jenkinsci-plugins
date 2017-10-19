5212742ac32bd6c979c26b8b4ba680c94b95c9d5
Oleksandr Rudak <Oleksandr_Rudak@epam.com>
2017-10-19 13:56:12 +0200

    Ininital import

---
+++ src/main/java/com/epam/drun/jenkins/plugins/JenkinsPluginMain.java
@@ -0,0 +1,13 @@
+package com.epam.drun.jenkins.plugins;
+
+public class JenkinsPluginMain {
+    public static void main(String[] args)
+    {
+        //do nothing;
+    }
+
+    public int sum(int a, int b)
+    {
+        return a + b;
+    }
+}
---
+++ src/test/java/com/epam/drun/jenkins/plugins/JenkinsPluginMainTest.java
@@ -0,0 +1,12 @@
+package com.epam.drun.jenkins.plugins;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class JenkinsPluginMainTest {
+
+    @Test
+    public void sum()
+    {}
+}
