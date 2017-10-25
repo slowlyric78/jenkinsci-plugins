83408ecec072388f587046763d4b466275b6942f
slowlyric89 <arudak@hotmail.com>
2017-10-25 14:46:52 +0200

    Added tests

--- src/test/java/com/epam/drun/jenkins/plugins/JenkinsPluginMainTest.java
+++ src/test/java/com/epam/drun/jenkins/plugins/JenkinsPluginMainTest.java
@@ -1,14 +1,41 @@
 package com.epam.drun.jenkins.plugins;
 
 import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
 
 import static org.junit.Assert.*;
 
 public class JenkinsPluginMainTest {
 
+    private JenkinsPluginMain instance = null;
+
+    @Before
+    public void setUp()
+    {
+        instance = new JenkinsPluginMain();
+    }
+
+    @After
+    public void tearDown()
+    {
+        instance = null;
+    }
+
     @Test
     public void sum()
     {
-        System.out.println("Output from the test");
+        final int a = 5;
+        final int b = 10;
+
+        int result = instance.sum (a, b);
+
+        assertEquals(15, result);
+    }
+
+    @Test
+    public void failingSum()
+    {
+        fail("Simulated failure");
     }
 }
